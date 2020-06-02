47efa8e02e9ca209b2ea2f87369e558ea6be85e4
王荣国 <wangrongguo@wangrongguodeMacBook-Pro.local>
2020-06-03 01:58:43 +0800

    electron mpv

---
+++ preload.js
@@ -0,0 +1,7 @@
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+//页面加载监听事件
+window.addEventListener('DOMContentLoaded', () => {
+  var videoDiv = document.getElementById("main");//播放器元素
+  videoDiv.innerHTML = 
+})
