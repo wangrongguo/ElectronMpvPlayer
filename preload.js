d75e8c1e858be8912d880e4c7a68013fa8ed60aa
王荣国 <wangrongguo@wangrongguodeMacBook-Pro.local>
2020-06-03 02:38:36 +0800

    测试提交Git

--- preload.js
+++ preload.js
@@ -1,5 +1,5 @@
 // All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
+// It has the same sandbox as a Chrome extension.测试提交git
 //页面加载监听事件
 window.addEventListener('DOMContentLoaded', () => {
   var videoDiv = document.getElementById("main");//播放器元素
